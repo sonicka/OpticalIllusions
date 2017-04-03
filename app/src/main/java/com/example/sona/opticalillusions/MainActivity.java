b79baffe416fc36459ed6553247b0a93c86dee60
Sona Leitmanova <sona.leitmanova@gmail.com>
2017-04-04 00:39:42 +0200

    simple main activity class created

---
+++ app/src/main/java/com/example/sona/opticalillusions/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.sona.opticalillusions;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Soňa on 04-Apr-17.
+ */
+
+public class MainActivity extends Activity{
+
+    @Override //TODO
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
