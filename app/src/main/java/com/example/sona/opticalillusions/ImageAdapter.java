9a6e6cd7714cb55dd83a0127fd29644a1907f55f
Sona Leitmanova <sona.leitmanova@gmail.com>
2017-04-05 16:29:06 +0200

    grid view working

--- app/src/main/java/com/example/sona/opticalillusions/ImageAdapter.java
+++ app/src/main/java/com/example/sona/opticalillusions/ImageAdapter.java
@@ -1,7 +1,6 @@
 package com.example.sona.opticalillusions;
 
 import android.content.Context;
-import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
