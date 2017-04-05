92ff9d3e38a2c2c7fcfbe57623d6358880647ca3
Sona Leitmanova <sona.leitmanova@gmail.com>
2017-04-05 16:28:03 +0200

    grid view working

--- app/src/main/java/com/example/sona/opticalillusions/IllusionsGridActivity.java
+++ app/src/main/java/com/example/sona/opticalillusions/IllusionsGridActivity.java
@@ -1,123 +1,20 @@
 package com.example.sona.opticalillusions;
 
 import android.app.Activity;
-
-import android.content.Context;
-import android.net.Uri;
-import android.os.Build;
 import android.os.Bundle;
-import android.provider.MediaStore;
-import android.support.annotation.RequiresApi;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.BaseAdapter;
 import android.widget.GridView;
-import android.widget.ImageView;
-import android.widget.LinearLayout;
-import java.util.ArrayList;
 
 public class IllusionsGridActivity extends Activity {
-
-    protected GridView mGridView;
-    protected ImageAdapter mAdapter;
-    private ArrayList<Integer> images = new ArrayList<>();
+    protected GridView gridView;
+    protected ImageAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_illusions_grid);
 
-        images.add(R.drawable.thumbcafewall);
-        images.add(R.drawable.thumbcolordifference01);
-        images.add(R.drawable.thumbcolordifference02);
-
-
-        /*gridview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            public void onItemClick(AdapterView<?> parent, View v,
-                                    int position, long id) {
-                Toast.makeText(IllusionsGridActivity.this, "" + position,
-                        Toast.LENGTH_SHORT).show();
-            }
-        });**/
-
-        // Get the GridView layout
-        mGridView = (GridView) findViewById(R.id.gridView);
-        mAdapter = new ImageAdapter(this);
-        mGridView.setAdapter(mAdapter);
-    }
-
-
-    class ImageAdapter extends BaseAdapter {
-
-        private Context mContext;
-
-        public ImageAdapter(Context context) {
-            mContext = context;
-        }
-
-        @Override
-        public int getCount() {
-            return images.size();
-        }
-
-        @Override
-        public Object getItem(int position) {
-            return null;
-        }
-
-        @Override
-        public long getItemId(int position) {
-            return 0;
-        }
-
-        @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN)
-        @Override
-        public View getView(int position, View convertView, ViewGroup parent) {
-            ImageView imageView;
-            int imageID = 0;
-
-            // Want the width/height of the items
-            // to be 120dp
-            int wPixel = dpToPx(120);
-            int hPixel = dpToPx(120);
-
-            if (convertView == null) {
-                // If convertView is null then inflate the appropriate layout file
-                convertView = LayoutInflater.from(mContext).inflate(R.layout.grid_item, null);
-            } else {
-
-            }
-
-            imageView = (ImageView) convertView.findViewById(R.id.imageView);
-
-            // Set height and width constraints for the image view
-            imageView.setLayoutParams(new LinearLayout.LayoutParams(wPixel, hPixel));
-
-            // Set the content of the image based on the provided URI
-            imageView.setImageURI(
-                    Uri.withAppendedPath(MediaStore.Images.Thumbnails.EXTERNAL_CONTENT_URI, "" + imageID)
-            );
-
-            // Image should be cropped towards the center
-            imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
-
-            // Set Padding for images
-            imageView.setPadding(8, 8, 8, 8);
-
-            // Crop the image to fit within its padding
-            imageView.setCropToPadding(true);
-
-            return convertView;
-        }
-
-        // Convert DP to PX
-        // Source: http://stackoverflow.com/a/8490361
-        public int dpToPx(int dps) {
-            final float scale = getResources().getDisplayMetrics().density;
-            int pixels = (int) (dps * scale + 0.5f);
-
-            return pixels;
-        }
+        gridView = (GridView) findViewById(R.id.gridView);
+        adapter = new ImageAdapter(this);
+        gridView.setAdapter(adapter);
     }
 }
--- app/src/main/java/com/example/sona/opticalillusions/ImageAdapter.java
+++ app/src/main/java/com/example/sona/opticalillusions/ImageAdapter.java
@@ -5,62 +5,57 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.GridView;
 import android.widget.ImageView;
-
-import com.example.sona.opticalillusions.R;
-
 import java.util.ArrayList;
 import java.util.List;
 
 final class ImageAdapter extends BaseAdapter {
-    private final List<Item> mItems = new ArrayList<Item>();
-    private final LayoutInflater mInflater;
+    private final List<Item> images = new ArrayList<>();
+    //private final LayoutInflater mInflater;
+    private Context context;
 
-    public ImageAdapter(Context context) {
-        mInflater = LayoutInflater.from(context);
+    public ImageAdapter(Context c) {
+        context = c;
+        //mInflater = LayoutInflater.from(context);
 
-        mItems.add(new Item("Red",       R.drawable.thumbcafewall));
-       /* mItems.add(new Item("Magenta",   R.drawable.thumbcolordifference01));
-        mItems.add(new Item("Dark Gray", R.drawable.thumbcolordifference02));
-        mItems.add(new Item("Gray",      R.drawable.thumbebbinghaus));
-        mItems.add(new Item("Green",     R.drawable.thumbhering));
-        mItems.add(new Item("Cyan",      R.drawable.thumbhermanngrid))*/
+        images.add(new Item ("Cafe wall", R.drawable.thumbcafewall));
+        images.add(new Item ("Hering", R.drawable.thumbhering));
+        images.add(new Item ("Ebbinghaus", R.drawable.thumbebbinghaus));
+        images.add(new Item ("Color difference 1", R.drawable.thumbcolordifference01));
+        images.add(new Item ("Color difference 2", R.drawable.thumbcolordifference02));
+        images.add(new Item ("Hermann Grid", R.drawable.thumbhermanngrid));
     }
-
 
     @Override
     public int getCount() {
-        return mItems.size();
+        return images.size();
     }
 
     @Override
     public Item getItem(int i) {
-        return mItems.get(i);
+        return null;
     }
 
     @Override
     public long getItemId(int i) {
-        return mItems.get(i).drawableId;
+        return 0;
     }
 
     @Override
-    public View getView(int i, View view, ViewGroup viewGroup) {
-        View v = view;
-        ImageView picture;
-
-        if (v == null) {
-            v = mInflater.inflate(R.layout.grid_item, viewGroup, false);
-            v.setTag(R.id.picture, v.findViewById(R.id.picture));
+    public View getView(int position, View convertView, ViewGroup viewGroup) {
+        ImageView imageView;
+        if (convertView == null) {
+            imageView = new ImageView(context);
+            imageView.setLayoutParams(new GridView.LayoutParams(300, 300));
+            imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
+            imageView.setPadding(20, 20, 20, 20);
+        } else {
+            imageView = (ImageView) convertView;
         }
 
-        picture = (ImageView) v.getTag(R.id.picture);
-
-
-        Item item = getItem(i);
-
-        picture.setImageResource(item.drawableId);
-
-        return v;
+        imageView.setImageResource(images.get(position).getDrawableId());
+        return imageView;
     }
 
     private static class Item {
@@ -71,55 +66,9 @@ final class ImageAdapter extends BaseAdapter {
             this.name = name;
             this.drawableId = drawableId;
         }
+
+        public int getDrawableId() {
+            return drawableId;
+        }
     }
 }
-
-/*package com.example.sona.opticalillusions;
-
-import android.content.Context;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.BaseAdapter;
-import android.widget.GridView;
-import android.widget.ImageView;
-
-public class ImageAdapter extends BaseAdapter {
-    private Context mContext;
-
-    public ImageAdapter(Context c) {
-        mContext = c;
-    }
-
-    public int getCount() {
-        return mThumbIds.length;
-    }
-
-    public Object getItem(int position) {
-        return null;
-    }
-
-    public long getItemId(int position) {
-        return 0;
-    }
-
-    // create a new ImageView for each item referenced by the Adapter
-    public View getView(int position, View convertView, ViewGroup parent) {
-        ImageView imageView;
-        if (convertView == null) {
-            imageView = new ImageView(mContext);
-            imageView.setLayoutParams(new GridView.LayoutParams(GridView.LayoutParams.WRAP_CONTENT, GridView.LayoutParams.WRAP_CONTENT));
-            imageView.setScaleType(ImageView.ScaleType.FIT_XY);
-            imageView.setPadding(8, 8, 8, 8);
-        } else {
-            imageView = (ImageView) convertView;
-        }
-
-        imageView.setImageResource(mThumbIds[position]);
-        return imageView;
-    }
-
-    private Integer[] mThumbIds = {
-            R.drawable.thumbcafewall, R.drawable.thumbcolordifference01,
-            R.drawable.thumbcolordifference02, R.drawable.thumbebbinghaus,
-            R.drawable.thumbhering, R.drawable.thumbhermanngrid};
-}*/
