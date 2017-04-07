3fdc2524b432875dc6b1ba6e684e5d3e0b917786
Sona Leitmanova <sona.leitmanova@gmail.com>
2017-04-07 16:05:08 +0200

    database

--- app/src/main/java/com/example/sona/opticalillusions/DatabaseContract.java
+++ app/src/main/java/com/example/sona/opticalillusions/DatabaseContract.java
@@ -12,18 +12,20 @@ public final class DatabaseContract {
     }
 
     public static class AllIllusions implements BaseColumns {
-        public static final String TABLE_NAME = "All";
+        public static final String TABLE_NAME = "all";
         public static final String COLUMN_NAME = "name";
         public static final String COLUMN_ID = "id";
+        public static final String COLUMN_CATEGORY = "category";
         public static final String COLUMN_DESCRIPTION = "description";
         public static final String COLUMN_PICTURE = "picture";
         public static final String COLUMN_ANIMATION = "animation";
     }
 
     public static class FavouriteIllusions implements BaseColumns {
-        public static final String TABLE_NAME = "Favourites";
+        public static final String TABLE_NAME = "favourites";
         public static final String COLUMN_NAME = "name";
         public static final String COLUMN_ID = "id";
+        public static final String COLUMN_CATEGORY = "category";
         public static final String COLUMN_DESCRIPTION = "description";
         public static final String COLUMN_PICTURE = "picture";
         public static final String COLUMN_ANIMATION = "animation";
